92484a274d184f5c0c37c0883a2f2712d0b176ff
Joel Singh <joelsingh788@gmail.com>
2025-05-25 01:41:38 -0400

    Source hyprland config in home.nix

--- home.nix
+++ home.nix
@@ -52,7 +52,7 @@
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".config/hypr/hyprland.conf".source = dotfiles/hyprland.conf;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
