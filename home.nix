9563f8f60b4fc02fafa7fbaa4b8aaeaff20e3084
Joel Singh <joelsingh788@gmail.com>
2025-05-25 02:46:29 -0400

    Install acpi for battery warning

--- home.nix
+++ home.nix
@@ -19,6 +19,7 @@
   # environment.
   home.packages = [
     pkgs.kitty
+    pkgs.acpi
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
