8705549ae11d27d381324ff8f4f337f54d16ca5b
Joel Singh <joelsingh788@gmail.com>
2025-06-08 16:17:08 -0400

    48 current  2025-06-08 16:17:03  25.11.20250520.2795c50     6.12.29                          *

--- modules/common-configuration.nix
+++ modules/common-configuration.nix
@@ -131,7 +131,7 @@
   ];
 
    # ircSession is the name of the new service we'll be creating
-   systemd.services.testTouch = {
+   systemd.services.pullQuteBrowser = {
       enable = true;
       # this service is "wanted by" (see systemd man pages, or other tutorials) the system 
       # level that allows multiple users to login and interact with the machine non-graphically 
@@ -142,15 +142,11 @@
       # of systemd nodes; here we are saying that we want our service to start after the network has 
       # set up (as our IRC client needs to relay over the network)
       after = [ "network.target" ];
-      description = "Test creating a file";
+      description = "Pull qute browser repo at startup";
       serviceConfig = {
-        # see systemd man pages for more information on the various options for "Type": "notify"
-        # specifies that this is a service that waits for notification from its predecessor (declared in
-        # `after=`) before starting
-        Type = "notify";
-        # username that systemd will look for; if it exists, it will start a service associated with that user
+        Type = "oneshot";
         User = "apple";
-        ExecStart = ''${pkgs.coreutils}/bin/touch /home/apple/repos/nixos/theserviceran''; 
+        ExecStart = ''cd /home/apple/.config/qutebrowser/; ${pkgs.git}/bin/git pull'';
         ExecStop = '''';
       };
    };
