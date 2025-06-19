803d9f31bc2c2206a19c4f11be9ff04930dcef22
Joel Singh <joelsingh788@gmail.com>
2025-06-19 12:04:59 -0400

    137 current  2025-06-19 11:57:52  25.11.20250520.2795c50     6.12.29                          *

--- config/scripts/rebuild-if-behind.sh
+++ config/scripts/rebuild-if-behind.sh
@@ -1,10 +1,8 @@
 set -e
 sleep 3 # Wait for internet
+
 cd /home/apple/repos/nixos/
+
 git fetch
-behind_info="$(git status | grep 'is behind')"
-if [[ -n "$behind_info" ]]; then
-  git pull
-  notify-send "starting rebuilld"
-  sudo nixos-rebuild switch --flake ~/repos/nixos\#$CURRENT_COMPUTER
-fi
+git pull
+sudo nixos-rebuild switch --flake ~/repos/nixos\#$CURRENT_COMPUTER
