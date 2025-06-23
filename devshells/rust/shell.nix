9625e0cdbdfba7beed020f7ef8b82e7249e9dc56
Joel Singh <joelsingh788@gmail.com>
2025-06-23 18:25:09 -0400

    160 current  2025-06-23 16:57:51  25.11.20250520.2795c50     6.12.29                          *

--- devshells/rust/shell.nix
+++ devshells/rust/shell.nix
@@ -7,10 +7,9 @@ mkShell rec {
     pkg-config
   ];
   buildInputs = [
-    alsa-lib-with-plugins vulkan-loader
+    udev alsa-lib-with-plugins vulkan-loader
     xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
     libxkbcommon wayland # To use the wayland feature
-    pkgsCross.mingw32.stdenv.cc
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
