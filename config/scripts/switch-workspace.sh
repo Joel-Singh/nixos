c0bf0c2cc0802b4e85b286776430dbdc95ac37bf
Joel Singh <joelsingh788@gmail.com>
2025-07-18 19:11:00 -0400

    149 current  2025-07-18 19:10:48  25.11.20250520.2795c50     6.12.29                          *

--- config/scripts/switch-workspace.sh
+++ config/scripts/switch-workspace.sh
@@ -1,9 +1,13 @@
-
 active=$(hyprctl -j monitors | jq --raw-output '.[] | select(.focused==true).specialWorkspace.name | split(":") | if length > 1 then .[1] else "" end') # From https://www.reddit.com/r/hyprland/comments/1b6bf39/how_to_close_special_workspace_when_switching_to/
 
+# Convert active to its length
 if [[ ${#active} -gt 0 ]]; then
   hyprctl dispatch togglespecialworkspace "$active"
 else
-  hyprctl dispatch workspace 2
+  activeWorkspace=$(hyprctl -j monitors | jq '.[0].activeWorkspace.id')
+  if [[ $activeWorkspace -eq 1 ]]; then
+    hyprctl dispatch workspace 2
+  else
+    hyprctl dispatch workspace 1
+  fi
 fi
-
