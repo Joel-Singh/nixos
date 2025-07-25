f93f8a59fe8d5fe64119c30e0c2e9ae702b8a9f4
Joel Singh <joelsingh788@gmail.com>
2025-07-24 20:40:03 -0400

    257 current  2025-07-24 20:39:46  25.11.20250520.2795c50     6.12.29                          *

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -18,10 +18,10 @@
     "$mainMod&Shift_L, J, swapwindow, d"
     "$mainMod&Shift_L, L, swapwindow, r"
 
-    "$mainMod&Alt_R, H, resizeactive, -10 0"
-    "$mainMod&Alt_R, K, resizeactive, 0 10"
-    "$mainMod&Alt_R, J, resizeactive, 0 -10"
-    "$mainMod&Alt_R, L, resizeactive, 10 0"
+    "$mainMod&Alt_R, H, resizeactive, -25 0"
+    "$mainMod&Alt_R, K, resizeactive, 0 25"
+    "$mainMod&Alt_R, J, resizeactive, 0 -25"
+    "$mainMod&Alt_R, L, resizeactive, 25 0"
 
     "Alt_L, Tab, exec, /home/apple/repos/nixos/config/scripts/switch-workspace.sh"
     "$mainMod, bracketright, exec, /home/apple/repos/nixos/config/scripts/move-workspace.sh"
