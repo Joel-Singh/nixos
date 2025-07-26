e5e10254f9e42edb89d42685171d9a6158909571
Joel Singh <joelsingh788@gmail.com>
2025-07-25 20:44:43 -0400

    279 current  2025-07-25 20:44:25  25.11.20250520.2795c50     6.12.29                          *

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -175,13 +175,13 @@
   # binde repeats on hold
   binde = [
     "$mainMod&Shift_R, H, moveactive, -5% 0"
-    "$mainMod&Shift_R, K, moveactive, 0 5%"
-    "$mainMod&Shift_R, J, moveactive, 0 -5%"
+    "$mainMod&Shift_R, K, moveactive, 0 -5%"
+    "$mainMod&Shift_R, J, moveactive, 0 5%"
     "$mainMod&Shift_R, L, moveactive, 5% 0"
 
     "$mainMod&Alt_R, H, resizeactive, -25 0"
-    "$mainMod&Alt_R, K, resizeactive, 0 25"
-    "$mainMod&Alt_R, J, resizeactive, 0 -25"
+    "$mainMod&Alt_R, K, resizeactive, 0 -25"
+    "$mainMod&Alt_R, J, resizeactive, 0 25"
     "$mainMod&Alt_R, L, resizeactive, 25 0"
   ];
   
